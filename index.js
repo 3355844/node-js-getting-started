587b77675d5c1a96a70bb0559d0ad1ad9aa3fb48
Andrei Ponomarenko <3355844@gmail.com>
2017-12-15 20:09:08 +0200

    change server from path to http server

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const app = express();
 const path = require('path');
+const http = require('http').Server(app);
 const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// express()
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//   .get('/', (req, res) => res.render('pages/index'))
+// express.listen(PORT, () = > console.log(`Listening on ${ PORT }`)
+// )
+// ;
+
+http.listen(5000, function () {
+    console.log('Listen on 5000 PORT... ');
+});
+
+app.get('/', function (req, res) {
+    console.log('Cone to get / URL and give index page');
+    res.render('pages/index');
+});
